5dac0c2b47db975b737837e6989fe72aa76c8c89
BWeddle <72270236+BWeddle@users.noreply.github.com>
2022-11-16 19:36:54 -0600

    part6?

--- routes/games.js
+++ routes/games.js
@@ -11,4 +11,7 @@ router.get('/detail', game_controlers.game_view_one_Page);
 /* GET create game page */ 
 router.get('/create', game_controlers.game_create_Page);
 
+/* GET create update page */ 
+router.get('/update', game_controlers.game_update_Page); 
+
 module.exports = router;
