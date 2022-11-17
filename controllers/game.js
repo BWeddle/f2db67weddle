7f0e9c0d0bf6c96c4742fa31c0776c53a8cd18aa
BWeddle <72270236+BWeddle@users.noreply.github.com>
2022-11-16 19:20:51 -0600

    part5

--- controllers/game.js
+++ controllers/game.js
@@ -98,3 +98,17 @@ exports.game_view_all_Page = async function(req, res) {
         res.send(`{'error': '${err}'}`); 
     } 
 }; 
+
+// Handle building the view for creating a costume. 
+// No body, no in path parameter, no query. 
+// Does not need to be async 
+exports.game_create_Page =  function(req, res) { 
+    console.log("create view") 
+    try{ 
+        res.render('gamecreate', { title: 'Game Create'}); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+}; 
--- routes/games.js
+++ routes/games.js
@@ -8,4 +8,7 @@ router.get('/', game_controlers.game_view_all_Page);
 /* GET detail game page */ 
 router.get('/detail', game_controlers.game_view_one_Page);
 
+/* GET create game page */ 
+router.get('/create', game_controlers.game_create_Page);
+
 module.exports = router;
