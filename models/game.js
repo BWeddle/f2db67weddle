8815b18591f55d69a3c197b1bf4f4a1952fdbb6f
BWeddle <72270236+BWeddle@users.noreply.github.com>
2022-11-30 19:35:13 -0600

    Done

--- models/game.js
+++ models/game.js
@@ -2,7 +2,15 @@ const mongoose = require("mongoose")
 const gameSchema = mongoose.Schema({ 
  game_name: String, 
  genre: String, 
- cost: Number 
+ cost: {
+    type: Number,
+    length: {
+        minimum: 1,
+        maximum: 150
+    }
+ }
 }) 
+
+
  
 module.exports = mongoose.model("Game", gameSchema)
